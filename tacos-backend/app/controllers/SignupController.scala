551acbcbb8585c042510f144eae0256ae3357620
Honahuku <tech@honahuku.com>
2023-05-31 01:29:33 +0900

    refactor: SignupController to use explicit request body type

--- tacos-backend/app/controllers/SignupController.scala
+++ tacos-backend/app/controllers/SignupController.scala
@@ -5,26 +5,20 @@ import play.api.mvc._
 import models.SignupRequest
 import models.SignupResponse
 import play.api.libs.json.Json
+import views.html.defaultpages.badRequest
+import play.api.libs.json._
 
 @Singleton
 class SignupController @Inject() (
     val controllerComponents: ControllerComponents
 ) extends BaseController {
 
-  def signup: Action[AnyContent] = Action {
-    implicit request: Request[AnyContent] =>
-      val json = request.body.asJson
-
-      json.flatMap(_.asOpt[SignupRequest]) match {
-        case Some(signupRequest) =>
-          // ターミナルにemailとpasswordを表示
-          println(
-              s"Email: ${signupRequest.email}, Password: ${signupRequest.password}"
-          )
-          // ここでsignupRequestを使って何か処理を行う
-          Ok(Json.toJson(SignupResponse("operation was successful!")))
-        case None =>
-          BadRequest(Json.toJson(SignupResponse("processing failed")))
-      }
+  def signup: Action[SignupRequest] = Action(parse.json[SignupRequest]) {
+    request =>
+      println(
+        s"Email: ${request.body.email}, Password: ${request.body.password}"
+      )
+      // ここでsignupRequestを使って何か処理を行う
+      Ok(Json.toJson(SignupResponse("operation was successful!")))
   }
 }
