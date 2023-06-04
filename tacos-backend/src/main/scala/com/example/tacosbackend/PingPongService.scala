e1b5ec57a16dc142fb1100a5defbbe17b3063c98
Honahuku <tech@honahuku.com>
2023-06-04 13:48:17 +0900

    fix: dependencies

--- tacos-backend/src/main/scala/com/example/tacosbackend/PingPongService.scala
+++ tacos-backend/src/main/scala/com/example/tacosbackend/PingPongService.scala
@@ -3,25 +3,38 @@ package com.example.tacosbackend
 import cats.effect._
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
-import io.circe.generic.auto._
+import org.http4s.implicits._
+import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.circe._
+import io.circe.generic.auto._
+import io.circe.Json
+import io.circe.syntax._
+import scala.concurrent.ExecutionContext
 
-case class Ping(value: String)
-case class Pong(value: String)
+case class Ping(msg: String)
+case class Pong(msg: String)
 
-class PingPongService extends Http4sDsl[IO] {
+object PingPongServer extends IOApp {
+  val dsl = Http4sDsl[IO]; import dsl._
 
-  implicit val pingDecoder = jsonOf[IO, Ping]
-  implicit val pongEncoder = jsonEncoderOf[IO, Pong]
-
-  val service: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case req @ POST -> Root / "ping" =>
-      req.as[Ping].flatMap { ping =>
-        if (ping.value == "ping") {
-          Ok(Pong("pong!"))
-        } else {
-          BadRequest("Invalid request")
+  val pingPongRoutes = HttpRoutes.of[IO] {
+    case req @ POST -> Root =>
+      req.decode[Json] { data =>
+        data.as[Ping] match {
+          case Right(ping) if ping.msg == "ping" => Ok(Pong("pong!").asJson)
+          case _ => BadRequest("Invalid message!")
         }
       }
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val pingPongApp = pingPongRoutes.orNotFound
+
+    BlazeServerBuilder[IO](ExecutionContext.global)
+      .bindHttp(8080, "localhost")
+      .withHttpApp(pingPongApp)
+      .resource
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
   }
 }
