80420ac27e442a5469f740eb149f8c2311807637
Honahuku <tech@honahuku.com>
2023-06-17 23:22:15 +0900

    add next project

---
+++ tacos-frontend/.eslintrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import('eslint/lib/shared/types').ConfigData} */
+modules.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+};
